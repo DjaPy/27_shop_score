92a0b56ead615dd4701cb33cef18aeea1fe06386
Sergey Ivanov <pdjapy@yandex.ru>
2018-02-19 02:04:10 +0300

    Cleans up the api and configures the robot.txt

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+import os
+
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
 DEBUG = False
 SQLALCHEMY_DATABASE_URI = 'postgresql://score:Rysherat2@shopscore' \
                           '.devman.org:5432/shop'
--- score/views.py
+++ score/views.py
@@ -1,21 +1,8 @@
-import json
+import datetime
 from flask import render_template, Response, send_from_directory, request
 from score.models import db
 from score.app import score_shop
 from score import query_db
-
-
-@score_shop.route('/api/score_information')
-def score_information():
-    json_data = {
-        'count_completed_orders': query_db.get_completed_orders(),
-        'count_fulfillment_orders': query_db.get_fulfillment_orders(),
-        'max_fulfillment_orders_delay': query_db.get_time_fulfillment_delay(),
-    }
-    response = Response(json.dumps(json_data),
-                        status=200,
-                        mimetype='application/json')
-    return response
 
 
 @score_shop.route('/')
@@ -23,10 +10,8 @@ def score():
     confirmed_orders_count = query_db.get_completed_orders()
     open_orders_count = query_db.get_fulfillment_orders()
     waiting_time = query_db.get_time_fulfillment_delay()
-
-
+    waiting_time = round(waiting_time, 1)
     return render_template('score.html',
-                           now=datetime.today(),
                            score=waiting_time,
                            orders_count=open_orders_count,
                            done_today=confirmed_orders_count,
@@ -35,4 +20,4 @@ def score():
 
 @score_shop.route('/robots.txt')
 def static_from_root():
-    return send_from_directory(score.static_folder, request.path[1:])
+    return send_from_directory(score_shop.static_folder, request.path[1:])
